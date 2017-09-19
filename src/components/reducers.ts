4b447b2cd1a041642f8e3f315fc2d35c43cbe59e
alan@theuhl.org <alan@theuhls.org>
2017-09-19 19:03:18 -0400

    interim commit

--- src/components/reducers.ts
+++ src/components/reducers.ts
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import { 
   Action, 
   VisibilityFilters,
@@ -11,11 +12,6 @@ interface Todo {
   completed: boolean;
   index: number;
 }
-
-const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-};
 
 // tslint:disable-next-line:no-any
 const todos = (state: any = [], action: Action) => {
@@ -50,52 +46,20 @@ const todos = (state: any = [], action: Action) => {
   }
 };
 
-// tslint:disable-next-line:no-any
-export const todoApp = (state: any = initialState, action: Action) => {
+const visibilityFilter = 
+  (state = VisibilityFilters.SHOW_ALL, action: Action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return Object.assign({}, state, { 
         visibilityFilter: action.filter 
-      });
-    case ADD_TODO:
-    case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: todos(state, action)
       });
     default:
       return state;
   }
 };
 
-// // tslint:disable-next-line:no-any
-// export function todoApp(state: any = initialState, action: Action) {
-//   switch (action.type) {
-//     case SET_VISIBILITY_FILTER:
-//       return Object.assign({}, state, {
-//         visibilityFilter: action.filter
-//       });
-//     case ADD_TODO:
-//       return Object.assign({}, state, {
-//         todos: [
-//           ...state.todos,
-//           {
-//             text: action.text,
-//             completed: false 
-//           }
-//         ]
-//       });
-//     case TOGGLE_TODO:
-//       return Object.assign({}, state, {
-// // tslint:disable-next-line:no-any
-//         todos: state.todos.map((todo: any, index: number) => {
-//           if (index === action.index) {
-//             return Object.assign({}, todo, {
-//               completed: !todo.completed
-//             });
-//           }
-//         })
-//       });
-//     default:
-//       return state;
-//   }
-// }
+// tslint:disable-next-line:no-any
+export const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
